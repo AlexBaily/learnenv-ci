0608718ca5f585190d283460e59bd8ab926d1f7e
AlexBaily <alex.d.baily@gmail.com>
2022-02-05 17:09:10 +0000

    Update script to include error checking.

--- infra/01.tf-plan.sh
+++ infra/01.tf-plan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+SCRIPT_ERR=0
 
 git clone https://github.com/AlexBaily/learnenv-infra
 cd learnenv-infra
@@ -7,12 +8,17 @@ git checkout $BRANCH
 
 cd terraform/environmnets/dev/networking/
 
-terraform init &> $WORKSPACE/output.txt
+terraform init -no-color &> $WORKSPACE/output.txt
 
 #Check to make sure init ran before trying to run the plan.
 if [ $? == 0 ]; then
     terraform plan -no-color &> $WORKSPACE/output.txt
+    if [ $? != 0 ]; then
+        SCRIPT_ERR=1
+    fi
     sed -i '/.*Refresh.*/d' $WORKSPACE/output.txt
+else
+    SCRIPT_ERR=1
 fi
 
 #Escape tilde for change in terraform.
@@ -25,3 +31,7 @@ echo "{\"body\":" > $WORKSPACE/parsed_output.txt
 sed ':a;N;$!ba;s/\n/<br \>/g' $WORKSPACE/output.txt | jq -aRs . >> $WORKSPACE/parsed_output.txt && echo "}" >> $WORKSPACE/parsed_output.txt
 
 curl -H "Authorization: token $GITHUB_TOKEN" "https://api.GitHub.com/repos/AlexBaily/learnenv-infra/issues/$pr_num/comments" -H "Accept: application/vnd.github.VERSION.text+json" -X POST -d "@$WORKSPACE/parsed_output.txt"
+
+if [ SCRIPT_ERR != 0 ]; then
+    exit 1
+fi
